6f86513c716424f03a713593558c6edbde6346f7
Rafael Lima <rafalimajo@gmail.com>
2021-03-14 21:15:23 -0300

    start

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1615767188,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/redpaddle-sheet0.png",
+		"images/bluepaddle-sheet0.png",
+		"images/ballspawn-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/leftgoal-sheet0.png",
+		"images/background.png",
+		"images/table-sheet0.png",
+		"media/ballsound.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
